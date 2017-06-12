dbc31395663f99268ae8416b5ea90958f72d1983
scotty-b <businge.scott@andela.com>
2017-06-12 23:13:00 +0300

    outline tests for authentication process

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,1 +1,58 @@
 __author__ = 'scotty'
+
+import unittest
+
+import json
+
+
+class AuthenticationTestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_register_successfully(self):
+        pass
+
+    def test_registration_route(self):
+        pass
+
+    def test_wrong_registration_route(self):
+        pass
+
+    def test_invalid_username(self):
+        pass
+
+    def test_wrong_password(self):
+        pass
+
+    def test_register_without_password(self):
+        pass
+
+    def test_register_with_special_characters(self):
+        pass
+
+    def register_without_username(self):
+        pass
+
+    def register_test_with_existing_username(self):
+        pass
+
+    def test_with_short_password(self):
+        pass
+
+    def test_login_successful(self):
+        pass
+
+    def test_login_blank_password(self):
+        pass
+
+    def test_login_blank_username(self):
+        pass
+
+    def test_login_with_invalid_details(self):
+        pass
+
+    def test_login_with_special_characters_username(self):
+        pass
