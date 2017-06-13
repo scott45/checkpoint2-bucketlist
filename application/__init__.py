59728d901b24b0e8ef44ef918f302fd27e22ce96
scotty-b <businge.scott@andela.com>
2017-06-13 16:35:49 +0300

    specify config and environment names in init file

--- application/__init__.py
+++ application/__init__.py
@@ -1,1 +1,13 @@
-__author__ = 'scotty'
+from flask import Flask
+
+from instance.config import application_configuration
+
+app = Flask(__name__)
+
+
+def EnvironmentName(environment):
+    app.config.from_object(application_configuration[environment])
+
+
+EnvironmentName('DevelopmentEnvironment')
+from application.v1 import bucketlist
