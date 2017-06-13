f42a6c1063bb4fd545761f613463f71545e73ecf
scotty-b <businge.scott@andela.com>
2017-06-13 18:02:27 +0300

    create the bucketlist model

--- application/v1/models.py
+++ application/v1/models.py
@@ -20,3 +20,21 @@ class Users(databases.Model):
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.hashed_password)
+
+
+class BucketList(databases.Model):
+    __tablename__ = 'Bucketlist'
+    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
+    user = databases.Column(databases.Integer, databases.ForeignKey('User.id'))
+    name = databases.Column(databases.String(150))
+    date_created = databases.Column(databases.DateTime, default=datetime.utcnow())
+    date_modified = databases.Column(databases.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    created_by = databases.Column(databases.Integer())
+
+    def __init__(self, name, created_by):
+        self.name = name
+        self.created_by = created_by
+
+    def save(self):
+        databases.session.add(self)
+        databases.session.commit
