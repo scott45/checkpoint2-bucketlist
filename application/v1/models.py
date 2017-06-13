4088ecb6ff1c8219f122146e544352aaba449dca
scotty-b <businge.scott@andela.com>
2017-06-13 18:29:03 +0300

    create the bucketlist-Item model

--- application/v1/models.py
+++ application/v1/models.py
@@ -26,7 +26,7 @@ class BucketList(databases.Model):
     __tablename__ = 'Bucketlist'
     id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
     user = databases.Column(databases.Integer, databases.ForeignKey('User.id'))
-    name = databases.Column(databases.String(150))
+    name = databases.Column(databases.Strinag(150))
     date_created = databases.Column(databases.DateTime, default=datetime.utcnow())
     date_modified = databases.Column(databases.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
     created_by = databases.Column(databases.Integer())
@@ -38,3 +38,20 @@ class BucketList(databases.Model):
     def save(self):
         databases.session.add(self)
         databases.session.commit()
+
+
+class Items(databases.Model):
+    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
+    name = databases.Column(databases.String(200))
+    date_created = databases.Column(databases.DateTime, default=datetime.utcnow())
+    date_modified = databases.Column(databases.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    bucketlist_id = databases.Column(databases.Integer, databases.ForeignKey('Bucketlist.id'))
+
+    def __init__(self, name, bucketlist_id):
+        self.name = name
+        self.bucketlist_id = bucketlist_id
+
+    def save(self):
+        databases.session.add(self)
+        databases.session.commit()
+
