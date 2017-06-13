32e8604ea407dca8add7dda76bfa785800422761
scotty-b <businge.scott@andela.com>
2017-06-13 17:40:37 +0300

    create the user model

--- application/v1/models.py
+++ application/v1/models.py
@@ -1,1 +1,22 @@
-__author__ = 'scotty'
+from datetime import datetime
+from passlib.apps import custom_app_context as pwd_context
+
+from application import databases
+
+
+class Users(databases.Model):
+    __tablename__ = 'User'
+    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
+    Bucket = databases.relationship('BucketList', backref='User')
+    username = databases.Column(databases.String(100))
+    password_hash = databases.Column(databases.String(200))
+
+    def save(self):
+        databases.session.add(self)
+        databases.session.commit()
+
+    def hash_password(self, password):
+        self.hashed_password = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.hashed_password)
