12be2380f401732bb07b06a3ab5a16231371e4d8
scotty-b <businge.scott@andela.com>
2017-06-13 15:35:14 +0300

    create the project environments in config file

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,29 @@
-__author__ = 'scotty'
+import os
+
+
+class MainConfiguration(object):
+    pass
+
+
+class TestingEnvironment(MainConfiguration):
+    DEBUG = True
+    TESTING = True
+
+
+class DevelopmentEnvironment(MainConfiguration):
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionEnvironment(MainConfiguration):
+    DEBUG = False
+    TESTING = False
+
+
+# Dictionary with keys mapping to the different configuration environments
+application_configuration = {
+    'MainConfiguration': MainConfiguration,
+    'TestingEnvironment': TestingEnvironment,
+    'DevelopmentEnvironment': DevelopmentEnvironment,
+    'ProductionEnvironment': ProductionEnvironment
+}
