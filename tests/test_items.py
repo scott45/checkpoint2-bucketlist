a4e84c327d536b5afedc6d568111cf55595f3241
scotty-b <businge.scott@andela.com>
2017-06-14 17:37:50 +0300

    outline tests methods for the bucketlist items

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,1 +1,47 @@
-__author__ = 'scotty'
+import unittest
+
+import json
+
+
+class BucketlistItemsTestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_new_bucketlistitem(self):
+        pass
+
+    def test_create_existing_bucketlistitem(self):
+        pass
+
+    def test_create_items_for_unexisting_bucketlist(self):
+        pass
+
+    def test_update_existing_bucketlistitem(self):
+        pass
+
+    def test_update_unexisting_bucketlistitem(self):
+        pass
+
+    def test_delete_bucketlistitem(self):
+        pass
+
+    def test_delete_unexisting_bucketlistitem(self):
+        pass
+
+    def test_get_one_bucketlistitem(self):
+        pass
+
+    def test_get_unexisting_bucketlistitem(self):
+        pass
+
+    def test_get_bucketlist_by_id(self):
+        pass
+
+    def test_get_bucketlist_by_invalid_id(self):
+        pass
+
+    def test_create_new_bucketlist_without_title(self):
+        pass
