fce36bf45da8ece38dc52fac71dc56514b1d6d1a
scotty-b <businge.scott@andela.com>
2017-06-16 11:06:03 +0300

    define setup and teardown functions for items tests

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,17 +1,38 @@
 import unittest
 
+# from config file
+from app import app, EnvironmentName, databases
+
 import json
 
 
 class BucketlistItemsTestCases(unittest.TestCase):
     def setUp(self):
-        pass
+        # testing client using testing environment
+
+        self.app = app.test_client()
+        EnvironmentName('TestingEnvironment')
+        databases.create_all()
+
+        # instance of a user directed to register route
+        payload = json.dumps({'username': 'scott', 'password': 'bucketlist'})
+        self.app.post('/bucketlist/api/v1/auth/register', data=payload)
+        credentials = self.app.post('/bucketlist/api/v1/auth/login', data=payload)
+        json_rep = json.loads(credentials.data)
+
+        # creating a bucketlist for testing purpose
+        self.token = json_rep['Token']
+        self.itempayloads = json.dumps({'name': 'Romans 8:38, Nothing can separate us from the love of God.'})
 
     def tearDown(self):
-        pass
+        databases.session.remove()
+        databases.drop_all()
 
     def test_create_new_bucketlistitem(self):
-        pass
+        response = self.app.post('bucketlist/api/v1/bucketlist/1/items',
+                                 data=self.itempayloads, headers={"Authorization": self.token})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Bucketlistitem successfully created", response.data.decode('utf-8'))
 
     def test_create_existing_bucketlistitem(self):
         pass
