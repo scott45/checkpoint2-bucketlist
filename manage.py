b529e1057f19838a6e0c1bc5ee0f6428d89a5d32
scotty-b <businge.scott@andela.com>
2017-06-16 23:51:20 +0300

    solve import error bug

--- manage.py
+++ manage.py
@@ -5,12 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app.v1 import models
-from app import app, databases
+from api.v1 import models
+from api.__init__ import app, databases
 
 migrate = Migrate(app, databases)
 manager = Manager(app)
 manager.add_command('databases', MigrateCommand)
-
-if __name__ == '__main__':
-    manager.run()
