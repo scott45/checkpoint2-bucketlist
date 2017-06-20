8ebc166c3da9676b6c14cb079cc8b73f70cf550d
scotty-b <businge.scott@andela.com>
2017-06-20 23:08:58 +0300

    add command manager decorators

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -11,3 +12,22 @@ from api.__init__ import app, databases
 migrate = Migrate(app, databases)
 manager = Manager(app)
 manager.add_command('databases', MigrateCommand)
+
+@manager.command
+def init_db():
+    os.system('createdb bucketlist_db')
+    os.system('createdb testing_db')
+    print('Databases created')
+
+
+@manager.command
+def drop_db():
+    os.system(
+        'psql -c "DROP DATABASE IF EXISTS test_db"')
+    os.system(
+        'psql -c "DROP DATABASE IF EXISTS bucketlist_db"')
+    print('Databases dropped')
+
+
+if __name__ == '__main__':
+    manager.run()
